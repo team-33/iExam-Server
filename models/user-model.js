0b02c942a26e98dac178257618e3789e06fc82b0
hirantha <sahanhirantha@gmail.com>
2019-04-14 08:44:09 +0530

    user mongoose model added

---
+++ models/user-model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    method: {
+        type: String,
+        //more methods will add
+        enum: ['google'],
+        required: true
+    },
+    google: {
+        google_id: {
+            type: String,
+        },
+        email: {
+            type: String,
+            lowercase: true
+        },
+        family_name:{
+            type:String
+        },
+        given_name:{
+            type:String
+        },
+        photo:{
+            type:String
+        }
+    }
+})
+
+module.exports = mongoose.model('user',userSchema);
