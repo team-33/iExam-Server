a5931752ad1797c7a02bbfc9ff6c22c42c05400e
hirantha <sahanhirantha@gmail.com>
2019-04-14 10:40:28 +0530

    authenticate midlleware created

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var authRouter = require('./auth/');
 
+var authMiddle = (req,res,next) => {
+    if(req.body.jwt) next();
+    else res.status(401).send({status:1200,message:'unauthorized'});
+}
+
 router.use('/auth',authRouter)
 
 module.exports = router;
