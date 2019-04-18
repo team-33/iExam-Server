c083d75bf63ad2586da3f22bbc37c73b252d040d
hirantha <sahanhirantha@gmail.com>
2019-04-18 22:07:09 +0530

    api added for get single paper details,still cant get answer details

--- routes/papers/index.js
+++ routes/papers/index.js
@@ -5,12 +5,32 @@ const passport = require('passport');
 const passportJWT = passport.authenticate('jwt', { session: false });
 const Paper = require('../../models/paper-model');
 
-router.get('/:subject/:year' ,(req,res,next) => {
+router.get('/:subject/:year' ,async (req,res,next) => {
+    var {subject,year} = req.params;
+    var aggregates = {
+        $match:{
+            subject:{
+                $regex:'^'+subject+'$',$options:'i'
+            },
+            year: +year
+        }
+    };
+    try{
+    var papers = await Paper.aggregate([aggregates]);
+    res.send(papers[0]);
+    } catch (e){
+        res.status(500).send(e.message);
+    }
     
 })
 router.get('/' ,async (req,res,next) => {
+    try{
     var papers = await Paper.find();
     res.send(papers);
+    } catch (e) {
+        console.log('error',e);
+        res.status(500).send('error');
+    }
 })
 
 module.exports = router;
